5a47463394497b6d58b9eae1b1ce30a5f351f37e
Alimhmmd <32460946+Alimhmmd@users.noreply.github.com>
2017-10-25 05:47:45 -0700

    Praktek0204.cpp

---
+++ Praktek0204.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double LuasRumah = 42;
+	double LantaiPkt = 193*1215;
+	double LuasBox = LantaiPkt*8/1000000;
+	double JumlahBoxP,BiayaP,BiayaPG,Total8; (
+	
+	cout << "Luas Rumah				: "<<LuasRumah<<" m2"<<endl;
+	cout << "Luas 1 Box				: "<<Luas1box<<" m2"<<endl;
+	
+	JumlahBoxP = LuasRumah/Luas1Box;
+	cout<<"Jumlah Box Parket		: "<<JumlahBoxP<<endl;
+	
+	BiayaP = JumlahBoxP*500;
+	cout<<"Biaya Parket (Rp)		: "<<BiayaP<<endl;
+	
+	BiayaPG = LuasRumah*20;
+	cout <<"Biaya Pasang (Rp)		: "<<BiayaPG<<endl;
+	
+	TotalB = BiayaP*BiayaPG;
+	cout <<"Total Biaya (Rp)		: "<<TotalB<<endl;
+}
